78f2ee3dd89f4f088f21893f6c3909d1e2e6a243
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2026-08-12 11:34:43 -0300

    tentativa 3 - Media

--- src/src/ValidatePathEncoding.php
+++ src/src/ValidatePathEncoding.php
@@ -10,29 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 class ValidatePathEncoding
 {
     /**
-     * Validate that the incoming request path is correctly URL-encoded
-     * and contains valid UTF-8 after decoding.
+     * Validate the encoding of the incoming request path.
      *
-     * @throws MalformedUrlException
+     * @throws \Illuminate\Http\Exceptions\MalformedUrlException
      */
     public function handle(Request $request, Closure $next): Response
     {
         $path = $request->path();
 
-        if ($this->hasMalformedPercentEncoding($path)) {
-            throw new MalformedUrlException;
-        }
-
-        if (! mb_check_encoding(rawurldecode($path), 'UTF-8')) {
+        if ($this->hasMalformedPercentEncoding($path) ||
+            ! mb_check_encoding(rawurldecode($path), 'UTF-8')) {
             throw new MalformedUrlException;
         }
 
         return $next($request);
     }
 
-    /**
-     * Determine whether the path contains an invalid percent-encoded sequence.
-     */
     private function hasMalformedPercentEncoding(string $path): bool
     {
         return preg_match('/%(?![0-9A-Fa-f]{2})/', $path) === 1;
